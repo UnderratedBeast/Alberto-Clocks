14395ea38248d3ad8b63151e8cfa359ef3565db2
Fawaz <owolabifawazenitan@gmial.com>
2024-12-10 11:34:49 +0100

    deployment changes made

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.PNG"],
-  assetsInclude: ["**/*.JPG"]
+  assetsInclude: ["**/*.PNG",
+    "**/*.JPG"
+  ]
 })
 
 
